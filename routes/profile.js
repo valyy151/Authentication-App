feb147b5633a98378fc5f37f9d439be8f486e546
valyy151 <marin.valenta1@gmail.com>
2023-02-25 06:58:57 +0100

    Remade the photos section (css)

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,26 @@
 const router = require('express').Router();
 const User = require('../models/User.model');
 const jwt = require('jsonwebtoken');
+const bycrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
 	res.render('profile');
+});
+
+router.post('/', async (req, res) => {
+	const { oldPass, newPass, confirmedNewPass } = req.body;
+
+	const token = req.cookies['Json Web Token'];
+	jwt.verify(token, process.env.SECRET, async (err, decodedToken) => {
+		const user = await User.findById(decodedToken.id);
+		const hashedPass = user.password;
+		const isValidPass = bycrypt.compare(oldPass, hashedPass);
+		if (isValidPass) {
+			const salt = bycrypt.genSalt();
+			const encryptedPass = bycrypt.hash(newPass, salt);
+			console.log(encryptedPass);
+		} else throw Error;
+	});
 });
 
 module.exports = router;
